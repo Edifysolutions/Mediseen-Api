2098378951ad8439b5fc4a7b28468f24818d8974
Artalia <micaiah.effiong@gmail.com>
2020-03-17 18:14:59 +0100

    clearing db

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', auth_and_check.requestAuthenticationToken, (req, res)=>{
 });
 
 // syncing server with database
-db.sequelizeInst.sync({force : true}).then(function(){
+db.sequelizeInst.sync({force : false}).then(function(){
 	server.listen(PORT, ()=>{
 		console.log(`server is running on port ${PORT}`);
 		console.log(`database return status ${DROP_DB}`, typeof DROP_DB)
